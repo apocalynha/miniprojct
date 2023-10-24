a8601943e99b8ea87826b46161e39438a48e9e84
apocalynha <adittamvan88@gmail.com>
2023-10-24 16:29:13 +0700

    crud

---
+++ middleware/auth.go
@@ -0,0 +1,78 @@
+package middleware
+
+import (
+	"fmt"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func JWTMiddleware() echo.MiddlewareFunc {
+	SecretKey := os.Getenv("JWT_KEY")
+	return echojwt.WithConfig(echojwt.Config{
+		SigningKey:    []byte(SecretKey),
+		SigningMethod: "HS256",
+	})
+}
+
+func CreateToken(userId int, name, role string) (string, error) {
+	SecretKey := os.Getenv("JWT_KEY")
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = userId
+	claims["name"] = name
+	claims["role"] = role
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() //Token expires after 1 hour
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(SecretKey))
+}
+
+func ExtractTokenUserRole(e echo.Context) string {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		role := claims["role"].(string)
+		return role
+	}
+	return "customer"
+}
+
+func HashPassword(password string) string {
+	result, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(result)
+}
+
+func ComparePassword(hash, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NotFoundHandler(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		err := next(c)
+
+		if err != nil {
+			if he, ok := err.(*echo.HTTPError); ok {
+				if he.Code == http.StatusNotFound {
+					errorMessage := "Invalid Endpoint"
+					return c.JSON(http.StatusNotFound, map[string]interface{}{
+						"message": errorMessage,
+					})
+				}
+			}
+
+			fmt.Println("Terjadi kesalahan:", err)
+		}
+
+		return err
+	}
+}
