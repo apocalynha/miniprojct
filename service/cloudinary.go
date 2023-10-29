cc1970c3273d1ad23a31fac23529b58124bf615e
apocalynha <adittamvan88@gmail.com>
2023-10-29 23:52:50 +0700

    change git action cloudinary

--- service/cloudinary.go
+++ service/cloudinary.go
@@ -6,14 +6,12 @@ import (
 	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
-	"log"
 	"os"
 )
 
 func CloudinaryUpload(c echo.Context, fileheader string) string {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("gagal akses file .env")
-	}
+	godotenv.Load(".env")
+	
 	fileHeader, _ := c.FormFile(fileheader)
 	file, _ := fileHeader.Open()
 	ctx := context.Background()
