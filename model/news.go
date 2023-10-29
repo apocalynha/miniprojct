e7c1eaea2fe937a6cad4199696939673ea16abdf
apocalynha <adittamvan88@gmail.com>
2023-10-29 13:35:16 +0700

    enhc: news response

--- model/news.go
+++ model/news.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type News struct {
@@ -11,21 +10,4 @@ type News struct {
 	User    User   `json:"user" gorm:"foreignkey:UserID"` // Explicitly define the foreign key
 	Tittle  string `json:"tittle" form:"tittle"`
 	Content string `json:"content" form:"content"`
-}
-
-type NewsResponse struct {
-	ID        uint      `json:"id"`
-	UpdatedAt time.Time `json:"updated_at"`
-	User      User      `json:"user" ` // Explicitly define the foreign key
-	Tittle    string    `json:"tittle" `
-	Content   string    `json:"content"`
-}
-
-func (newsDB News) ResponseConvert() NewsResponse {
-	var Response NewsResponse
-	Response.ID = newsDB.ID
-	Response.Tittle = newsDB.Tittle
-	Response.Content = newsDB.Content
-
-	return Response
 }
